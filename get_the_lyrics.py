bd2bffa7290ff654a4cbefbd58861607091b3d40
Kacper Szczygieł <kac.szczygiel@gmail.com>
2023-06-29 15:21:32 +0200

    seccond commint

---
+++ get_the_lyrics.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+
+
+class Lyrics:
+    def __init__(self, path):
+        self.path = path
+
+    def lyrics(self):
+        page = requests.get("https://genius.com/" + self.path)
+        soup = BeautifulSoup(page.text, 'html.parser')
+        div = soup.find(class_="Lyrics__Container-sc-1ynbvzw-5 Dzxov")
+        for br in div.find_all("br"):
+            br.replace_with("\n")
+        return print(re.sub('<[^<]+?>', '', str(div)))

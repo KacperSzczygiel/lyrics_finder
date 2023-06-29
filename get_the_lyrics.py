b54b31445f46d795a865b767d0de7860333b3137
Kacper Szczygieł <kac.szczygiel@gmail.com>
2023-06-29 16:08:33 +0200

    seccond commint

--- get_the_lyrics.py
+++ get_the_lyrics.py
@@ -8,7 +8,7 @@ class Lyrics:
         self.path = path
 
     def lyrics(self):
-        page = requests.get("https://genius.com/" + self.path)
+        page = requests.get("https://genius.com" + self.path)
         soup = BeautifulSoup(page.text, 'html.parser')
         div = soup.find(class_="Lyrics__Container-sc-1ynbvzw-5 Dzxov")
         for br in div.find_all("br"):
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from get_the_lyrics import Lyrics
 from find_path import Path
 
 client_id = "zcBdtGYUjGLXuoUtcr0Gfe3WyTl9lOjoYsZHqLu9uGylOHnDuo2dvfUa7mfZp0fb"
@@ -5,4 +6,6 @@ client_secret = "vXro5SAhey2kD85II9KxUVsBaaabXz2cv8hJEHPg_uwJqzy6DGcGC_2SbiF576V
 access_token = "n7662RkbzNvb5TD8g3hwohLEW264nnYvr9J65iianH46QjPXFbOCezLBYW3mVegI"
 
 song = Path(client_id, client_secret, access_token)
-song.search_song()
+path = song.search_the_song()
+lyrics = Lyrics(path)
+lyrics.lyrics()
